a510e326466a5f0d341c6105ca017f884a266b92
Desarae Veit <wbrown.web@gmail.com>
2018-09-04 13:24:31 -0500

    the start of a couple email php files

--- inClass/emailFunction.php
+++ inClass/emailFunction.php
@@ -1,8 +1,12 @@
 <?php
+
+function sendMail() {
 $to      = 'wbrown84@gmail.com';
 $subject = 'PHP Test Email';
 $message = 'It Is Working';
 $headers = 'From: contact@willbdesigned.com' . "\r\n";
-
 mail($to, $subject, $message, $headers);
+}
+
+sendMail();
 ?>
